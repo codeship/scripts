12170f38633d4fc0203c806fc94eaf686423e6df
Marko Locher <marko@locher.at>
2015-05-08 16:47:21 +0200

    Add information on SSH key

--- deployments/aptible.sh
+++ deployments/aptible.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 # Deploy to a Aptible, https://www.aptible.com/
 #
+# Before you use this script, add the public SSH key from your projects General
+# settings page to https://dashboard.aptible.com/settings/ssh
+#
 # Add this as an environment variable to your project configuration
 APTIBLE_APP=""
 
