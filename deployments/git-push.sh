0078355a05cf3ee0b07a2d22a8dcafbb9568f1d9
Marko Locher <marko@locher.at>
2015-05-08 11:29:49 +0200

    Much simpler version to push

--- deployments/git-push.sh
+++ deployments/git-push.sh
@@ -4,26 +4,12 @@
 # You can either add those here, or configure them on the environment tab of your
 # project settings. Please make sure the remote is configured in a way you can
 # push to, most likely you want to use a SSH based URL.
-USERNAME=""
-EMAIL=""
 REMOTE="git@github.com:USER/REPOSITORY.git"
 BRANCH="gh-pages"
-COMMIT_MESSAGE="Pushing from Codeship"
-
-# basic git configuration
-git config --global user.name "${USERNAME}"
-git config --global user.email "${EMAIL}"
 
 # fetch the missing history
 git fetch --unshallow || true
 
-# add the remote repository
+# push to the remote repository
 git remote add codeship_push "${REMOTE}"
-
-# checkout the branch and commit the changes
-git checkout -b "${BRANCH}" "${REMOTE}/${BRANCH}"
-git add -A .
-git commit -m "${COMMIT_MESSAGE}"
-
-# do the actual push
-git push codeship_push "${BRANCH}"
+git push codeship_push "${COMMIT_ID}:refs/heads/${BRANCH}"
