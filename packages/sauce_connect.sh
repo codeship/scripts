de2594020bfd88d0309b237899eb61a554766b45
Marko Locher <marko@locher.at>
2015-05-11 12:20:55 +0200

    Improvements for the sauce_connect script
    * Check for required environment variables
    * Update to latest version
    * bring in line with the ElasticSearch script

--- packages/sauce_connect.sh
+++ packages/sauce_connect.sh
@@ -1,22 +1,28 @@
 #!/bin/sh
 # Install an run the Sauce Connect server, https://saucelabs.com/
 #
-# Add the following items as environment variables to your project configuration
-# on Codeship.
-SAUCE_USER=""
-SAUCE_API_KEY=""
-SAUCE_VERSION="4.3.6"
+# Add at least the following environment variables to your project configuration
+# (otherwise the defaults below will be used).
+# * SAUCE_USER
+# * SAUCE_API_KEY
+#
+# Include in your builds via
+# https://raw.githubusercontent.com/codeship/scripts/master/packages/sauce_connect.sh | bash -s
+SAUCE_VERSION=${SAUCE_VERSION:="4.3.8"}
+SAUCE_DIR=${SAUCE_DIR:="$HOME/sc"}
+
+# check required parameters
+${SAUCE_USER:?'You need to configure the SAUCE_USER environment variable!'}
+${SAUCE_API_KEY:?'You need to configure the SAUCE_API_KEY environment variable!'}
 
 # exit on the first failure
 set -e
-SAUCE_DIR="${HOME}/sauce"
-SAUCE_DOWNLOAD="${HOME}/cache/sc-${SAUCE_VERSION}-linux.tar.gz"
+CACHED_DOWNLOAD="${HOME}/cache/sc-${SAUCE_VERSION}-linux.tar.gz"
 
 mkdir -p "${SAUCE_DIR}"
 rm -f "${SAUCE_DIR}/sc_ready"
-
-wget -O "${SAUCE_DOWNLOAD}" --continue "https://saucelabs.com/downloads/sc-${SAUCE_VERSION}-linux.tar.gz"
-tar --strip-components=1 -C "${SAUCE_DIR}" -xzf "${SAUCE_DOWNLOAD}"
+wget --continue --output-document "${CACHED_DOWNLOAD}" "https://saucelabs.com/downloads/sc-${SAUCE_VERSION}-linux.tar.gz"
+tar -xaf "${CACHED_DOWNLOAD}" --strip-components=1 --directory "${SAUCE_DIR}"
 
 ${SAUCE_DIR}/bin/sc -u "${SAUCE_USER}" -k "${SAUCE_API_KEY}" -f "${SAUCE_DIR}/sc_ready" 2>&1 &
 while [ ! -e "${SAUCE_DIR}/sc_ready" ]; do sleep 1; done
