6b42b1c59ba6227dc915846b66b15091c50a7921
Marko Locher <marko@locher.at>
2015-05-11 17:00:52 +0200

    Update the description and provide sample values

--- deployments/git-push.sh
+++ deployments/git-push.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Deploy to via pushing to a remote git repository.
 #
-# Add the following environment variables to your project configuration and add
-# the public SSH key from your projects General settings page to
-# https://dashboard.aptible.com/settings/ssh
-# * REMOTE_REPOSITORY
-# * REMOTE_BRANCH
+# Add the following environment variables to your project configuration and make
+# sure the public SSH key from your projects General settings page is allowed to
+# push to the remote repository as well.
+# * REMOTE_REPOSITORY, e.g. "git@github.com:codeship/documentation.git"
+# * REMOTE_BRANCH, e.g. "production"
 #
 # Include in your builds via
 # https://raw.githubusercontent.com/codeship/scripts/master/deployments/git-push.sh | bash -s
