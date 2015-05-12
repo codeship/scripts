1dcd65a0b2702daba687df440c9d37dd34668dc7
Marko Locher <marko@locher.at>
2015-05-12 11:21:13 +0200

    Update CloudFoundry deployment script
    * Switch to cf binary (previous versions aren't included on Codeship any longer)
    * Check environment variables

--- deployments/cloud_foundry.sh
+++ deployments/cloud_foundry.sh
@@ -1,19 +1,27 @@
-#!/bin/sh
-# Deploy to a CloudFoundry compatible API
+#!/bin/bash
+# Deploy to a CloudFoundry compatible API endpoint.
 #
-# You can either add those here, or configure them on the environment tab of your
-# project settings.
-CF_API=""
-CF_USER=""
-CF_PASSWORD=""
-CF_ORG=""
-CF_SPACE=""
-CF_APPLICATION=""
+# Add the following environment variables to your project configuration.
+# * CF_API
+# * CF_USER
+# * CF_PASSWORD
+# * CF_ORG
+# * CF_SPACE
+# * CF_APPLICATION
+#
+# Include in your builds via
+# https://raw.githubusercontent.com/codeship/scripts/master/deployments/cloud_foundry.sh | bash -s
+CF_API=${CF_API:?'You need to configure the CF_API environment variable!'}
+CF_USER=${CF_USER:?'You need to configure the CF_USER environment variable!'}
+CF_PASSWORD=${CF_PASSWORD:?'You need to configure the CF_PASSWORD environment variable!'}
+CF_ORG=${CF_ORG:?'You need to configure the CF_ORG environment variable!'}
+CF_SPACE=${CF_SPACE:?'You need to configure the CF_SPACE environment variable!'}
+CF_APPLICATION=${CF_APPLICATION:?'You need to configure the CF_APPLICATION environment variable!'}
 
 # Fail the deployment on the first error
 set -e
 
-cf6 api "${CF_API}"
-cf6 auth "${CF_USER}" "${CF_PASSWORD}"
-cf6 target -o "${CF_ORG}" -s "${CF_SPACE}"
-cf6 push "${CF_APPLICATION}"
+cf api "${CF_API}"
+cf auth "${CF_USER}" "${CF_PASSWORD}"
+cf target -o "${CF_ORG}" -s "${CF_SPACE}"
+cf push "${CF_APPLICATION}"
