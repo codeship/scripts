66d7c14dc588a6be001a964660fc5382d3c8033b
Marko Locher <marko@locher.at>
2015-05-22 13:55:21 +0200

    Simplify the installations script

--- packages/meteor.sh
+++ packages/meteor.sh
@@ -3,7 +3,4 @@
 #
 # Include in your builds via
 # \curl -sSL https://raw.githubusercontent.com/codeship/scripts/master/packages/meteor.sh | bash -s
-set -e
-curl https://install.meteor.com > "${HOME}/install_meteor"
-sed -i'' -e 's/PREFIX=.*/PREFIX="${HOME}"/g' "${HOME}/install_meteor"
-bash "${HOME}/install_meteor"
+\curl -sSL https://install.meteor.com | sed -e 's/PREFIX=.*/PREFIX="${HOME}"/g' | sh
