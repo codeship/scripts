80f754521441e3f652c8e568525d1d5351d5c078
Marko Locher <marko@locher.at>
2015-05-28 14:06:46 +0200

    Adapt the tests to check codeship.com

--- test.sh
+++ test.sh
@@ -29,4 +29,5 @@ echo "Testing scripts for custom packages"
 
 echo "Testing utility scripts"
 source utilities/random_timezone.sh
-bash utilities/check_url.sh https://google.com
+bash utilities/check_url.sh https://codeship.com
+! bash utilities/check_url.sh https://does_not_exist.codeship.com
