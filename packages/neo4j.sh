f855b2a6a889e2b2d6d0710289830a6fe2bef11e
Marko Locher <marko@locher.at>
2015-06-22 16:53:51 +0200

    Install a custom version of Neo4J

--- packages/neo4j.sh
+++ packages/neo4j.sh
@@ -13,13 +13,9 @@ NEO4J_DIR=${NEO4J_DIR:="$HOME/neo4j"}
 set -e
 CACHED_DOWNLOAD="${HOME}/cache/neo4j-community-${NEO4J_VERSION}-unix.tar.gz"
 
-rm -rf "${NEO4J_DIR}"
+mkdir -p "${NEO4J_DIR}"
 wget --continue --output-document "${CACHED_DOWNLOAD}" "http://neo4j.com/artifact.php?name=neo4j-community-${NEO4J_VERSION}-unix.tar.gz"
 tar -xaf "${CACHED_DOWNLOAD}" --strip-components=1 --directory "${NEO4J_DIR}"
 
-# configure neo4j to run on testing ports (7476 + 7475)
-sed -i s/7474/7476/g "${NEO4J_DIR}/conf/neo4j-server.properties"
-sed -i s/7473/7475/g "${NEO4J_DIR}/conf/neo4j-server.properties"
-
 # start the server
-"${NEO4J_DIR}/bin/neo4j start"
+"${NEO4J_DIR}/bin/neo4j" start
