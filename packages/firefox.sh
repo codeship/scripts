691e1cb865158b697ba2e3dae187854d6874aa87
Marko Locher <marko@locher.at>
2015-08-22 12:18:11 +0200

    Enhance the Firefox install script

--- packages/firefox.sh
+++ packages/firefox.sh
@@ -1,13 +1,20 @@
-#!/bin/sh
-# Install a custom version of Firefox
+#!/bin/bash
+# Install a custom version of Firefox, https://www.mozilla.org/en-US/firefox/new/
 #
-# You can either add this here, or configure them on the environment tab of your
-# project settings.
-FIREFOX_VERSION="28.0"
+# Add at least the following environment variables to your project configuration
+# (otherwise the defaults below will be used).
+# * FIREFOX_VERSION
+#
+# Include in your builds via
+# \curl -sSL https://raw.githubusercontent.com/codeship/scripts/master/packages/firefox.sh | bash -s
+FIREFOX_VERSION=${FIREFOX_VERSION:="28.0"}
+FIREFOX_DIR=${FIREFOX_DIR:="$HOME/firefox"}
 
+# fail the script on the first failing command.
 set -e
 CACHED_DOWNLOAD="${HOME}/cache/firefox-${FIREFOX_VERSION}.tar.bz2"
 
-rm -rf "${HOME}/firefox"
+rm -rf "${FIREFOX_DIR}"
+mkdir -p ${FIREFOX_DIR}
 wget --continue --output-document "${CACHED_DOWNLOAD}" "https://ftp.mozilla.org/pub/mozilla.org/firefox/releases/${FIREFOX_VERSION}/linux-x86_64/en-US/firefox-${FIREFOX_VERSION}.tar.bz2"
-tar -xaf "${CACHED_DOWNLOAD}" --directory "${HOME}"
+tar -xaf "${CACHED_DOWNLOAD}" --strip-components=1 --directory "${FIREFOX_DIR}"
