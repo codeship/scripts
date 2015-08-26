8ef9aa0531bb583d0cbf28baef91a75c7a3fcce4
Marko Locher <marko@locher.at>
2015-08-26 17:29:46 +0200

    Remove "set -e" from the (sourced) golang script

--- languages/go.sh
+++ languages/go.sh
@@ -9,13 +9,14 @@
 # \curl -sSL https://raw.githubusercontent.com/codeship/scripts/master/languages/go.sh > ${HOME}/go.sh && source ${HOME}/go.sh
 GO_VERSION=${GO_VERSION:="1.4.2"}
 
-# check required parameters
-export GOROOT="/tmp/go"
-export PATH="${GOROOT}/bin:${PATH}"
-
-set -e
+# no set -e because this file is sourced and with the option set a failing command
+# would cause an infrastructur error message on Codeship.
 CACHED_DOWNLOAD="${HOME}/cache/go${GO_VERSION}.linux-amd64.tar.gz"
 
 mkdir -p "${GOROOT}"
 wget --continue --output-document "${CACHED_DOWNLOAD}" "https://storage.googleapis.com/golang/go${GO_VERSION}.linux-amd64.tar.gz"
 tar -xaf "${CACHED_DOWNLOAD}" --strip-components=1 --directory "${GOROOT}"
+
+# configure the new GOROOT and PATH
+export GOROOT="/tmp/go"
+export PATH="${GOROOT}/bin:${PATH}"
