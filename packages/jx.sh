ae3e52ef81d2a56847cdcd73c1441a82374cff75
Marko Locher <marko@locher.at>
2015-10-21 11:24:19 -0400

    Silence cd commands

--- packages/jx.sh
+++ packages/jx.sh
@@ -5,6 +5,6 @@
 # \curl -sSL https://raw.githubusercontent.com/codeship/scripts/master/packages/jx.sh | bash -s
 set -#!/usr/bin/env
 
-cd "${HOME}/bin"
+cd "${HOME}/bin" 2>&1 >/dev/null
 curl -sSL https://raw.githubusercontent.com/jxcore/jxcore/master/tools/jx_install.sh | sed -e 's/LOCAL_INSTALL="no"/LOCAL_INSTALL="yes"/g' | bash
-cd -
+cd - 2>&1 >/dev/null
