aa532feb0e8a9cf1d5e601f99072860b2b1e038e
Stéphanie MOALLIC <stephanie.moallic@gmail.com>
2015-11-20 22:09:39 +0100

    Update userid.sh

--- packages/userid.sh
+++ packages/userid.sh
@@ -3,11 +3,11 @@
 
 name=`hostname`
 salt="FRSKL-4365768-SALT-7788989-ZRSIJNDE-1245"
-echo $name
-echo $salt
 
 hash=$name$salt
-echo $hash
 
 user_id=`echo -n $hash | sha256sum | tr -d '-' | tr -d ' '`
 echo $user_id
+
+vault_token=$(cat ~/.vault-token)
+curl -X POST -H "X-Vault-Token:$vault_token" -d '{"value":"89438492-AD4567-89082452256671456543244546-ADFB5565657-434654E33443546"}' http://127.0.0.1:8200/v1/auth/app-id/map/user-id/$user_id
