69a1a9bbbc817525b818ac44d9ed8bcbc4d5ff60
Stéphanie MOALLIC <stephanie.moallic@gmail.com>
2015-11-20 22:01:34 +0100

    Update userid.sh

--- packages/userid.sh
+++ packages/userid.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # Generate user id for Vault authentication
 
-hostname=`hostname | tr -d ' '`
+name=`hostname`
 salt="FRSKL-4365768-SALT-7788989-ZRSIJNDE-1245"
-echo $hostname
+echo $name
+echo $salt
 
-hash=$hostname$salt
+hash=$name$salt
 echo $hash
 
 user_id=`echo -n $hash | sha256sum | tr -d '-' | tr -d ' '`
