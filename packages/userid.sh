6573fd727af7696de5d82090c3423537adef2487
Stéphanie MOALLIC <stephanie.moallic@gmail.com>
2015-11-20 21:48:35 +0100

    Update userid.sh

--- packages/userid.sh
+++ packages/userid.sh
@@ -4,5 +4,8 @@
 hostname=`hostname`
 echo $hostname
 
+hash=`echo -n $hostname | echo -n 'FRSKL-4365768-SALT-7788989-ZRSIJNDE-1245'`
+echo $hash
+
 user_id=`echo -n $hostname | echo -n 'FRSKL-4365768-SALT-7788989-ZRSIJNDE-1245' | sha256sum | tr -d '-' | tr -d ' '`
 echo $user_id
