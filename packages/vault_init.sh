328157a817a5d6ae267650c3a68a55100463e21f
Stéphanie MOALLIC <stephanie.moallic@gmail.com>
2015-11-21 16:22:30 +0100

    Update vault_init.sh

--- packages/vault_init.sh
+++ packages/vault_init.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+# Initialize Vault authentication
+#
+name=`hostname`
+salt="FRSKL-4365768-SALT-7788989-ZRSIJNDE-1245"
+
+hash=$name$salt
+
+# generate user id
+user_id=`echo -n $hash | sha256sum | tr -d '-' | tr -d ' '`
+echo $user_id
+
+vault_token=$(cat ~/.vault-token)
+
+# enable authentication backend
+curl -X POST -H "X-Vault-Token:$vault_token" -d '{"type":"app-id"}' http://127.0.0.1:8200/v1/sys/auth/app-id
+
+# configure vault server app-id
+curl -X POST -H "X-Vault-Token:$vault_token" -d '{"value":"root", "display_name":"demo_test"}' http://127.0.0.1:8200/v1/auth/app-id/map/app-id/89438492-AD4567-89082452256671456543244546-ADFB5565657-434654E33443546
+
+# configure vault server user id
+curl -X POST -H "X-Vault-Token:$vault_token" -d '{"value":"89438492-AD4567-89082452256671456543244546-ADFB5565657-434654E33443546"}' http://127.0.0.1:8200/v1/auth/app-id/map/user-id/$user_id
