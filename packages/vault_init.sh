b1d8222f43014ae160587334655f4ddb7da7263f
Steffy29 <stephanie.moallic@gmail.com>
2015-11-25 11:09:59 +0100

    feat(config) : update config
    update config to launch local server

--- packages/vault_init.sh
+++ packages/vault_init.sh
@@ -7,13 +7,15 @@ salt="FRSKL-4365768-SALT-7788989-ZRSIJNDE-1245"
 hash=$name$salt
 
 # generate user id
-user_id=`echo -n $hash | sha256sum | tr -d '-' | tr -d ' '`
+user_id=`echo -n $hash | shasum | tr -d '-' | tr -d ' '`
+echo $user_id
+user_id="ca1beab9a9a421359282e5eb3c2a366836199e8f881c6210c570d13bfcf5e0a3"
 echo $user_id
 
 vault_token=$(cat ~/.vault-token)
 
 # enable authentication backend
-curl -X POST -H "X-Vault-Token:$vault_token" -d '{"type":"app-id"}' http://127.0.0.1:8200/v1/sys/auth/app-id
+# curl -X POST -H "X-Vault-Token:$vault_token" -d '{"type":"app-id"}' http://127.0.0.1:8200/v1/sys/auth/app-id
 
 # configure vault server app-id
 curl -X POST -H "X-Vault-Token:$vault_token" -d '{"value":"root", "display_name":"demo_test"}' http://127.0.0.1:8200/v1/auth/app-id/map/app-id/89438492-AD4567-89082452256671456543244546-ADFB5565657-434654E33443546
