8b71629465ab9fc4e84951db84b737f7f3b1cde2
Marko Locher <marko@locher.at>
2016-08-03 12:28:09 +0200

    Checkout git LFS contents when installing
    * Make sure to run fetch and checkout files stored via LFS
    * Update the default version to 1.3.1

--- packages/git-lfs.sh
+++ packages/git-lfs.sh
@@ -3,10 +3,11 @@
 #
 # Include in your builds via
 # \curl -sSL https://raw.githubusercontent.com/codeship/scripts/master/packages/git-lfs.sh | bash -s
-GIT_LFS_VERSION="1.1.0"
+GIT_LFS_VERSION="1.3.1"
 
 set -e
 GIT_LFS_DIR=${GIT_LFS_DIR:="$HOME/git-lfs"}
+REPO_DIR=$(readlink -f "${HOME}/clone")
 CACHED_DOWNLOAD="${HOME}/cache/git-lfs-linux-amd64-${GIT_LFS_VERSION}.tar.gz"
 
 mkdir -p "${GIT_LFS_DIR}"
@@ -15,4 +16,9 @@ tar -xaf "${CACHED_DOWNLOAD}" --strip-components=1 --directory "${GIT_LFS_DIR}"
 
 cd "${GIT_LFS_DIR}"
 PREFIX=${HOME} ./install.sh
+cd -
+
+cd "${REPO_DIR}"
+git lfs fetch
+git lfs checkout
 cd -
