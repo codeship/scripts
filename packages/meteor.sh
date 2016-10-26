e32bc99f3bbc82399ed5f030f2cd28bbb4442f4e
Florian Schaeffler <flo@hikey.io>
2016-10-26 12:39:42 +0300

    added meteor version as environment parameter

--- packages/meteor.sh
+++ packages/meteor.sh
@@ -3,4 +3,9 @@
 #
 # Include in your builds via
 # \curl -sSL https://raw.githubusercontent.com/codeship/scripts/master/packages/meteor.sh | bash -s
-\curl -sSL https://install.meteor.com | sed -e 's/PREFIX=.*/PREFIX="${HOME}"/g' | sh
+# 
+# Read your Meteor version via
+# export METEOR_RELEASE=$(cat .meteor/release | sed "s/METEOR@//g")
+
+METEOR_RELEASE=${METEOR_RELEASE:=""}
+\curl -sSL https://install.meteor.com | if [[ $METEOR_RELEASE]]; then sed -i 's/^RELEASE=.*$/RELEASE="'${METEOR_RELEASE}'"/g' fi | sed -e 's/PREFIX=.*/PREFIX="${HOME}"/g' | sh
