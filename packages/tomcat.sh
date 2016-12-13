a61421c1e72a3063d3653bb6fc6dec5d371b87bd
Jason Souza <jason@codeship.com>
2016-12-13 13:40:37 -0500

    adding some changes

--- packages/tomcat.sh
+++ packages/tomcat.sh
@@ -9,17 +9,17 @@
 # \curl -sSL https://raw.githubusercontent.com/codeship/scripts/master/packages/tomcat.sh | bash -s
 
 TOMCAT_VERSION=${TOMCAT_VERSION:="8.5.9"}
-TOMCAT_DIR=${TOMCAT_DIR="$HOME/tocat"}
+TOMCAT_DIR=${TOMCAT_DIR="$HOME/tomcat"}
 TOMCAT_WAIT_TIME=${TOMCAT_WAIT_TIME:="10"}
 
 set -e
 CACHED_DOWNLOAD="${HOME}/cache/apache-tomcat-${TOMCAT_VERSION}.tar.gz"
 
 mkdir -p "${TOMCAT_DIR}"
-wget --continue --output-document "${CACHED_DOWNLOAD}" "http://www-us.apache.org/dist/tomcat/tomcat-8/v${TOMCAT_VERSION}/bin/apache-tomcat-${TOMCAT_VERSION}.tar.gz"
-tar -xaf "${CACHED_DOWNLOAD}" --directory "${TOMCAT_DIR}"
+wget --continue --output-document "${CACHED_DOWNLOAD}" "http://www-us.apache.org/dist/tomcat/tomcat-${TOMCAT_VERSION:0:1}/v${TOMCAT_VERSION}/bin/apache-tomcat-${TOMCAT_VERSION}.tar.gz"
+tar -xaf "${CACHED_DOWNLOAD}" --strip-components=1 --directory "${TOMCAT_DIR}"
 
 # Make sure to use the exact parameters you want for Tomcat and give it enough sleep time to properly start up
-bash ${TOMCAT_DIR}/bash apache-tomcat-${TOMCAT_VERSION}/bin/startup.sh
+bash ${TOMCAT_DIR}/apache-tomcat-${TOMCAT_VERSION}/bin/startup.sh
 sleep ${TOMCAT_SLEEP_TIME}
 cd -
