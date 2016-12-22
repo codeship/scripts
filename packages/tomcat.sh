3b2944b36e93506c80382149644b8d33fc2fcee3
Jason Souza <jason@codeship.com>
2016-12-22 10:05:08 -0500

    adding condition to restart tomcat

--- packages/tomcat.sh
+++ packages/tomcat.sh
@@ -11,6 +11,8 @@
 TOMCAT_VERSION=${TOMCAT_VERSION:="8.5.9"}
 TOMCAT_DIR=${TOMCAT_DIR="$HOME/tomcat"}
 TOMCAT_WAIT_TIME=${TOMCAT_WAIT_TIME:="10"}
+# Set to 1 to restart tomcat
+TOMCAT_AUTOSTART=${TOMCAT_AUTOSTART:="1"}
 
 set -e
 CACHED_DOWNLOAD="${HOME}/cache/apache-tomcat-${TOMCAT_VERSION}.tar.gz"
@@ -19,9 +21,10 @@ mkdir -p "${TOMCAT_DIR}"
 wget --continue --output-document "${CACHED_DOWNLOAD}" "http://www-us.apache.org/dist/tomcat/tomcat-${TOMCAT_VERSION:0:1}/v${TOMCAT_VERSION}/bin/apache-tomcat-${TOMCAT_VERSION}.tar.gz"
 tar -xaf "${CACHED_DOWNLOAD}" --strip-components=1 --directory "${TOMCAT_DIR}"
 
-# Make sure to use the exact parameters you want for Tomcat and give it enough sleep time to properly start up
-bash ${TOMCAT_DIR}/bin/startup.sh
-sleep ${TOMCAT_WAIT_TIME}
-
+if [ "$TOMCAT_AUTOSTART" -eq "1" ]; then
+  # Make sure to use the exact parameters you want for Tomcat and give it enough sleep time to properly start up
+  bash ${TOMCAT_DIR}/bin/startup.sh
+  sleep ${TOMCAT_WAIT_TIME}
+fi
 # check if tomcat successfully installs
 bash ${TOMCAT_DIR}/bin/version.sh | grep "Apache Tomcat/${TOMCAT_VERSION}"
