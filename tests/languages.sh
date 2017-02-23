f71712e4ea6d77bef0a2ff7fadf6a3fae54a5f31
Joe Siewert <joe.siewert@codeship.com>
2017-02-23 14:17:11 -0700

    Refactor tests

---
+++ tests/languages.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -e
+
+# Dart
+export DART_VERSION="1.22.1"
+bash packages/dart.sh
+dart --version 2>&1 | grep "${DART_VERSION}"
+
+# Erlang
+export ERLANG_VERSION="19.2"
+source languages/erlang.sh
+erl -eval '{ok, Version} = file:read_file(filename:join([code:root_dir(), "releases", erlang:system_info(otp_release), "OTP_VERSION"])), erlang:display(erlang:binary_to_list(Version)), halt().' -noshell | grep "${ERLANG_VERSION}"
+
+# Elixir, requires the Erlang script above
+export ELIXIR_VERSION="1.4.2"
+source languages/elixir.sh
+elixir --version | grep "${ELIXIR_VERSION}"
+
+# Go
+export GO_VERSION="1.8"
+source languages/go.sh
+go version | grep "${GO_VERSION}"
+
+# Python 2.*
+export PYTHON_VERSION="2.7.13"
+source languages/python.sh
+python --version 2>&1 | grep "${PYTHON_VERSION}"
+rm -rf "${HOME}/pyenv"
+
+# Python 3.*
+export PYTHON_VERSION="3.6.0"
+source languages/python.sh
+python --version 2>&1 | grep "${PYTHON_VERSION}"
+
+# R
+export R_VERSION="3.3.2"
+source languages/r.sh
+R --version | grep "${R_VERSION}"
+
+# Rust
+source languages/rust.sh
+rustc --version
---
+++ tests/packages.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+set -e
+
+# ChromeDriver
+export CHROMEDRIVER_VERSION="2.27"
+bash packages/chromedriver.sh
+chromedriver --version | grep "${CHROMEDRIVER_VERSION}"
+
+# Firefox
+export FIREFOX_VERSION="51.0.1"
+bash packages/firefox.sh
+firefox --version | grep "${FIREFOX_VERSION}"
+
+# Git
+export GIT_VERSION="2.11.1"
+bash packages/git.sh
+git --version | grep "${GIT_VERSION}"
+
+# Git LFS
+export GIT_LFS_VERSION="1.5.6"
+bash packages/git-lfs.sh
+git lfs version | grep "git-lfs/${GIT_LFS_VERSION}"
+
+# ImageMagick
+export IMAGEMAGICK_VERSION="7.0.5-0"
+bash packages/imagemagick.sh
+identify -version | grep "${IMAGEMAGICK_VERSION}"
+
+# MongoDB
+export MONGODB_VERSION="3.4.0"
+bash packages/mongodb.sh
+netstat -lnp | grep "${MONGODB_PORT}.*mongod"
+
+# Neo4j
+export NEO4J_VERSION="2.2.2"
+bash packages/neo4j.sh
+netstat -lnp | grep "7473.*java"
+netstat -lnp | grep "7474.*java"
+
+# Phalcon PHP framework
+export PHALCON_VERSION="3.0.3"
+phpenv local 5.6
+bash packages/phalcon.sh
+php -m | grep phalcon
+
+# sbt
+export SBT_VERSION="0.13.8"
+bash packages/sbt.sh
+sbt --version | grep "${SBT_VERSION}"
+
+# Selenium Server
+export SELENIUM_VERSION="2.46.0"
+export SELENIUM_PORT="4444"
+bash packages/selenium_server.sh
+netstat -lnp | grep "${SELENIUM_PORT}.*java"
+
+# Haskell Stack
+export HASKELL_STACK_VERSION="1.3.2"
+bash packages/stack.sh
+stack --version | grep "${HASKELL_STACK_VERSION}"
+
+# Tomcat
+export TOMCAT_VERSION="8.5.11"
+bash packages/tomcat.sh
+bash ${HOME}/tomcat/bin/version.sh | grep "Apache Tomcat/${TOMCAT_VERSION}"
