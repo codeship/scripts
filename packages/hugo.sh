fef82b9089ec4ea2478c73072cbda9cfe309a77e
Chad Nierenhausen <cnieren@email.arizona.edu>
2017-04-11 14:30:49 -0700

    Upgrade Hugo installer to version 20

--- packages/hugo.sh
+++ packages/hugo.sh
@@ -3,14 +3,14 @@
 #
 # You can either add this here, or configure them on the environment tab of your
 # project settings.
-HUGO_VERSION="0.14"
+HUGO_VERSION="0.20"
 HUGO_DIR=${HUGO_DIR:="$HOME/hugo"}
 
 set -e
 CACHED_DOWNLOAD="${HOME}/cache/hugo_${HUGO_VERSION}_linux_amd64.tar.gz"
 
 mkdir -p "${HUGO_DIR}"
-wget --continue --output-document "${CACHED_DOWNLOAD}" "https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_linux_amd64.tar.gz"
+wget --continue --output-document "${CACHED_DOWNLOAD}" "https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz"
 tar -xaf "${CACHED_DOWNLOAD}" --strip-components=1 --directory "${HUGO_DIR}"
 ln -s "${HUGO_DIR}/hugo_${HUGO_VERSION}_linux_amd64" "${HOME}/bin/hugo"
 
