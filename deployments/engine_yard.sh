db25cfb3424f543a3f6301f450eb4b27117e4e53
Drew Kitch <drew.kitch@gmail.com>
2018-01-29 14:58:58 -0700

    keep process scoped to bash script

--- deployments/engine_yard.sh
+++ deployments/engine_yard.sh
@@ -7,10 +7,25 @@
 # * EY_APP_URL, (optional, will be checked for a HTTP/2xx status code if provided
 #
 # Include in your builds via
-# source /dev/stdin <<< "$(curl -sSL https://raw.githubusercontent.com/codeship/scripts/master/deployments/engine_yard.sh)"
-EY_API_TOKEN=${EY_API_TOKEN:?'You need to configure the EY_API_TOKEN environment variable!'}
+# \curl -sSL https://raw.githubusercontent.com/codeship/scripts/master/deployments/engine_yard.sh | bash -s
+
+set -e
+
+# Load RVM into a shell session *as a function* -- https://rvm.io/workflow/scripting
+if [[ -s "$HOME/.rvm/scripts/rvm" ]] ; then
+  # First try to load from a user install
+  source "$HOME/.rvm/scripts/rvm"
+elif [[ -s "/usr/local/rvm/scripts/rvm" ]] ; then
+  # Then try to load from a root install
+  source "/usr/local/rvm/scripts/rvm"
+else
+  printf "ERROR: An RVM installation was not found.\n"
+  false
+fi
 
 rvm use
+
+EY_API_TOKEN=${EY_API_TOKEN:?'You need to configure the EY_API_TOKEN environment variable!'}
 ENVIRONMENT_PARAMETER=${EY_ENVIRONMENT:+"--environment=$EY_ENVIRONMENT"}
 CHECK_URL_COMMAND=${EY_APP_URL:+"check_url $EY_APP_URL"}
 
