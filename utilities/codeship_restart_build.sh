742b239d1fcae38c3715a96e93c6952b79270eee
Drew Kitch <drew.kitch@gmail.com>
2018-10-19 09:48:01 -0600

    Update codeship_restart_build.sh

--- utilities/codeship_restart_build.sh
+++ utilities/codeship_restart_build.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env ruby
 
-# attempts to mimic behavior of original v1 API restart CodeShip build script
+# Maintains parity with behavior of original v1 API restart CodeShip build script -- https://github.com/codeship/scripts/blob/a2ba7093ce69c507ba5d997a38c506f4ed026087/utilities/codeship_restart_build.sh
+# Now back in operation with v2 API -- https://apidocs.codeship.com/v2
+
+# Since this script requires the generation of an access token with a CodeShip user/password
+# we recommend that you create a CodeShip user for the sole purpose of providing access to restart builds.
+
+# Your machine user can be relegated to a team with 'Contributor' role,
+# but do be sure the team itself is authorized to access this particular CodeShip project.
+
+# Here's our documentation for more information on managing teams -- https://documentation.codeship.com/general/account/organizations/#managing-teams-and-projects
 
 require 'json'
 require 'net/http'
@@ -40,7 +49,7 @@ class CodeshipRestartBuild
     @org_name = ENV['CODESHIP_ORG_NAME'] || abort("Please provide your CodeShip org name as an environment variable: CODESHIP_ORG_NAME=YOUR_CODESHIP_ORG_NAME")
     @project_uuid = ENV['CODESHIP_API_PROJECT_UUID'] || abort("Please provide your project uuid as an environment variable: CODESHIP_API_PROJECT_UUID=YOUR_CODESHIP_API_PROJECT_UUID")
     @branch_name = ENV['CODESHIP_API_BRANCH'] || abort("Please provide your branch name as an environment variable: CODESHIP_API_BRANCH=YOUR_CODESHIP_API_BRANCH")
-    @machine_user_email = ENV['CODESHIP_MACHINE_USER_EMAIL'] || abort("Please generate a new CodeShip user with limited permissions and provide password as environment variable: CODESHIP_MACHINE_USER_EMAIL=YOUR_MACHINE_USER_EMAIL")
+    @machine_user_email = ENV['CODESHIP_MACHINE_USER_EMAIL'] || abort("Please generate a new CodeShip user with limited permissions and provide email as environment variable: CODESHIP_MACHINE_USER_EMAIL=YOUR_MACHINE_USER_EMAIL")
     @machine_user_password = ENV['CODESHIP_MACHINE_USER_PASSWORD'] || abort("Please generate a new CodeShip user with limited permissions and provide password as environment variable: CODESHIP_MACHINE_USER_PASSWORD=YOUR_MACHINE_USER_PASSWORD")
     @started_session = start_session
     @access_token = @started_session[:token]
