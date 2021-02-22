codeship/scripts

commit 9c905d5f838d697fbe98769bc7b03f2277391f1a

Update download link for 2.x

packages/elasticsearch.sh
@@ -33,7 +33,7 @@ then
   ELASTICSEARCH_PLUGIN_BIN="${ELASTICSEARCH_DIR}/bin/elasticsearch-plugin"
 elif [ ${ELASTICSEARCH_VERSION:0:1} -eq 2 ]
 then
-  ELASTICSEARCH_DL_URL="https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/tar/elasticsearch/${ELASTICSEARCH_VERSION}/elasticsearch-${ELASTICSEARCH_VERSION}.tar.gz"
+  ELASTICSEARCH_DL_URL="https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/tar/elasticsearch/${ELASTICSEARCH_VERSION}/elasticsearch-${ELASTICSEARCH_VERSION}.tar.gz"
   ELASTICSEARCH_PLUGIN_BIN="${ELASTICSEARCH_DIR}/bin/plugin"
 else
   ELASTICSEARCH_DL_URL="https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-${ELASTICSEARCH_VERSION}.tar.gz"

commit f46ee07f246dbb9b4974593520f84d8790bbaadc

Fixed that distro download stops on sporadic 404 error
There is an intermittent issue with Elasticsearch installations (most likely started after 9c905d5f838d697fbe98769bc7b03f2277391f1a),
when elasticsearh.sh sometimes gets 404 from ES CDN, while next time it downloading it w/o any issues.

The ideal would be to use --retry-on-http-error:

wget --retry-on-http-error=404 --tries=10 --waitretry=3 \
    --continue --output-document "${CACHED_DOWNLOAD}" "${ELASTICSEARCH_DL_URL}"

However, this doesn't help on practice, wget was failing with 404 all 10 attempts:
```
--2021-02-22 16:04:02--  https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.10.0-linux-x86_64.tar.gz
Resolving artifacts.elastic.co (artifacts.elastic.co)... 34.120.127.130, 2600:1901:0:1d7::
Connecting to artifacts.elastic.co (artifacts.elastic.co)|34.120.127.130|:443... connected.
HTTP request sent, awaiting response... 404 Not Found
Retrying.

--2021-02-22 16:04:03--  (try: 2)  https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.10.0-linux-x86_64.tar.gz
Reusing existing connection to artifacts.elastic.co:443.
HTTP request sent, awaiting response... 404 Not Found
Retrying.

...

--2021-02-22 16:04:26--  (try:10)  https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.10.0-linux-x86_64.tar.gz
Reusing existing connection to artifacts.elastic.co:443.
HTTP request sent, awaiting response... 404 Not Found
Giving up.
```

My theory is that wget in this mode does not try to reconnect, uses the same IP to fetch file from,
(and yeah the root cause is somewhere in codeship network DNS, artifacts.elastic.co domain I bet is resolving onto old IP in one of your routers)

So switched here to shell loop, and that helped:
```
Making attempt # 1
--2021-02-22 16:16:59--  https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.10.0-linux-x86_64.tar.gz
Resolving artifacts.elastic.co (artifacts.elastic.co)... 34.120.127.130, 2600:1901:0:1d7::

packages/elasticsearch.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+
 # Install a custom ElasticSearch version - https://www.elastic.co/products/elasticsearch
 #
 # To run this script in Codeship, add the following
@@ -17,6 +19,7 @@ ELASTICSEARCH_VERSION=${ELASTICSEARCH_VERSION:="1.5.2"}
 ELASTICSEARCH_PORT=${ELASTICSEARCH_PORT:="9333"}
 ELASTICSEARCH_DIR=${ELASTICSEARCH_DIR:="$HOME/el"}
 ELASTICSEARCH_PLUGINS=${ELASTICSEARCH_PLUGINS:=""}
+MAX_ATTEMPTS=10
 
 # The download location of version 5.x and above, and 2.x follows a different URL structure than 1.x.
 # Make sure to use Oracle JDK 8 for Elasticsearch 5.x and above - run the following commands in your setup steps:
@@ -39,12 +42,29 @@ else
   ELASTICSEARCH_DL_URL="https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-${ELASTICSEARCH_VERSION}.tar.gz"
   ELASTICSEARCH_PLUGIN_BIN=""
 fi
-set -e
 
 CACHED_DOWNLOAD="${HOME}/cache/elasticsearch-${ELASTICSEARCH_VERSION}.tar.gz"
 
 mkdir -p "${ELASTICSEARCH_DIR}"
-wget --continue --output-document "${CACHED_DOWNLOAD}" "${ELASTICSEARCH_DL_URL}"
+
+ATTEMPT_NO=1
+while true; do
+    echo "Making attempt #" $ATTEMPT_NO
+    set +e
+    wget --continue --output-document "${CACHED_DOWNLOAD}" "${ELASTICSEARCH_DL_URL}"
+    if [ $? -eq 0 ]; then
+        # no any error from wget, break the loop
+        break
+    fi
+    set -e
+    if [ $ATTEMPT_NO -ge $MAX_ATTEMPTS ]; then
+        echo "Giving up downloading $ELASTICSEARCH_DL_URL after $ATTEMPT_NO attempts"
+        exit 404
+    fi
+    sleep 1
+    ATTEMPT_NO=$(expr $ATTEMPT_NO + 1)
+done
+
 tar -xaf "${CACHED_DOWNLOAD}" --strip-components=1 --directory "${ELASTICSEARCH_DIR}"
 
 echo "http.port: ${ELASTICSEARCH_PORT}" >> ${ELASTICSEARCH_DIR}/config/elasticsearch.yml