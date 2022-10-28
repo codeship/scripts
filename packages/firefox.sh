9395a7795e7b82e3412b24becd88750ac2285601
Joe Sak <joe@joesak.com>
2022-10-27 21:03:08 -0500

    Update default firefox version, fix URL

--- packages/firefox.sh
+++ packages/firefox.sh
@@ -7,7 +7,7 @@
 #
 # Include in your builds via
 # \curl -sSL https://raw.githubusercontent.com/codeship/scripts/master/packages/firefox.sh | bash -s
-FIREFOX_VERSION=${FIREFOX_VERSION:="28.0"}
+FIREFOX_VERSION=${FIREFOX_VERSION:="106.0"}
 FIREFOX_DIR=${FIREFOX_DIR:="$HOME/firefox"}
 
 # fail the script on the first failing command.
@@ -16,5 +16,5 @@ CACHED_DOWNLOAD="${HOME}/cache/firefox-${FIREFOX_VERSION}.tar.bz2"
 
 rm -rf "${FIREFOX_DIR}"
 mkdir -p ${FIREFOX_DIR}
-wget --continue --output-document "${CACHED_DOWNLOAD}" "https://ftp.mozilla.org/pub/mozilla.org/firefox/releases/${FIREFOX_VERSION}/linux-x86_64/en-US/firefox-${FIREFOX_VERSION}.tar.bz2"
+wget --continue --output-document "${CACHED_DOWNLOAD}" "https://ftp.mozilla.org/pub/firefox/releases/${FIREFOX_VERSION}/linux-x86_64/en-US/firefox-${FIREFOX_VERSION}.tar.bz2"
 tar -xaf "${CACHED_DOWNLOAD}" --strip-components=1 --directory "${FIREFOX_DIR}" --no-check-certificate
