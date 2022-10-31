575cb05cf3d41cc31a45ffd1f7d1d1609391c2df
Joe Sak <joe@joesak.com>
2022-10-31 10:55:43 -0600

    Fix Firefox download URL (#286)
    * Update firefox.sh
    
    * Update default firefox version, fix URL
    
    * Update firefox.sh
    
    * Set default back to 28
    
    Unlikely codeship would accept changing this default as it would break for customers relying on it

--- packages/firefox.sh
+++ packages/firefox.sh
@@ -16,5 +16,5 @@ CACHED_DOWNLOAD="${HOME}/cache/firefox-${FIREFOX_VERSION}.tar.bz2"
 
 rm -rf "${FIREFOX_DIR}"
 mkdir -p ${FIREFOX_DIR}
-wget --continue --output-document "${CACHED_DOWNLOAD}" "https://ftp.mozilla.org/pub/mozilla.org/firefox/releases/${FIREFOX_VERSION}/linux-x86_64/en-US/firefox-${FIREFOX_VERSION}.tar.bz2"
+wget --continue --output-document "${CACHED_DOWNLOAD}" "https://ftp.mozilla.org/pub/firefox/releases/${FIREFOX_VERSION}/linux-x86_64/en-US/firefox-${FIREFOX_VERSION}.tar.bz2"
 tar -xaf "${CACHED_DOWNLOAD}" --strip-components=1 --directory "${FIREFOX_DIR}"
